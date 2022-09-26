b706b5bf554c463720cc415d1077a99c97d4f746
Mohd Mubsshir Khan <kmubsshir@hotmail.com>
2022-09-26 22:31:36 +0530

    Add project files.

---
+++ TextReading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+namespace ReadTextFiles
+{
+    public class TextReader
+    {
+        public static void Main(String[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder("<Root>\n");
+            string path = @"C:\\Users\\Mubsshir\\source\\repos\\TextReading\\TextReading\\hello.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            string DPath = fileInfo.DirectoryName + "\\";
+            Console.WriteLine(DPath);
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                StreamReader streamReader = new StreamReader(fs);
+                string? line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    stringBuilder.Append("  <Student ");
+                    string[] words = line.Split(',');
+                    foreach (string word in words)
+                    {
+                        stringBuilder.Append(word + " ");
+                    }
+                    stringBuilder.Append("/>\n");
+                }
+                streamReader.Close();
+            }
+            stringBuilder.Append("</Root>");
+            string XML = stringBuilder.ToString();
+            using (FileStream newFile = new FileStream(DPath + "new.xml", FileMode.OpenOrCreate))
+            {
+                StreamWriter sw = new StreamWriter(newFile);
+                sw.Write(XML);
+                sw.Close();
+            }
+            Console.WriteLine(XML);
+        }
+    }
+}
